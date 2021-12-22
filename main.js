b64b903858cd00269aa71566081afeadfe6ec9fa
Moazzam Begg <Moazzam1@Moazzams-Air.cogeco.local>
2021-12-22 12:49:29 -0500

    completed

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 /* 
 👉 Write your kata here!
 // 
-//scenario: 
-you find yourself alone in the jungle facing a group of lions ready to attack you, 
-but luckly you have a rifle 
-with some ammo. In order to survive you have to kill all the lions before your
- ammo and time run out otherwise you are 
-dead. You can only shoot one lion per second.
-Create a function that  predict the result which takes in the number of lions, 
-the time you have and number of ammo.
-If you died, it will return "sorry!, you are dead".
-If not, it will return "congratulations! you survived!"
+//Scenario: 
+
+You find yourself alone in the jungle facing a group of lions ready to attack you, 
+but luckily you have a rifle with some ammo! In order to survive you have to kill all the lions before your
+ammo and time runs out otherwise you are dead. You can only shoot one lion per second.
+
+Create a function that predict the results which takes in the number of lions, the time you have and number of ammo.
+If you die, it will return "Sorry!, you are dead, you've run out of ammo" or "Sorry!, you are dead, you've run of time "
+If you surive, it will return "Congratulations! you survived!"
 
 you died if...
 ammo < lions 
@@ -22,4 +21,16 @@ ammo < lions
 // number of lions 
 // time  15 seconds
 
-//👉 Write the function your CodeWarriors will start with below here:
+/* Write the function your CodeWarriors will start with below here: */
+
+export function lionAttack(ammo, time, numberOfLions){
+    if (ammo>= numberOfLions && time >= numberOfLions){
+        return "Congratulations! you survived!"}
+    if(ammo < numberOfLions){
+        return "Sorry!, you are dead, you've run out of ammo"}
+    if (time < numberOfLions){
+        return "Sorry!, you are dead, you've run of time "
+}}
+
+
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,49 @@
 //👉 Write your tests below here:
+import {lionAttack} from "./main.js"
+describe("lionAttack", function (){
+
+    test("You should survive if ammo is more than the number of lions and time is greater or equal to lion",
+    function(){
+        //Arrange
+        const ammo = 20
+        const time = 15
+        const numberOfLions = 10
+        
+        const expected = "Congratulations! you survived!"
+        //Act
+        const actual = lionAttack(ammo, time, numberOfLions);
+        //Assert
+        expect(actual).toBe(expected)
+    })
+
+    test("You should not survive if ammo is less than the number of lions",
+    function(){
+        //Arrange
+        const ammo = 5
+        const time = 15
+        const numberOfLions = 10
+        const expected = "Sorry!, you are dead, you've run out of ammo"
+        //Act
+        const actual = lionAttack(ammo, time, numberOfLions);
+        //Assert
+        expect(actual).toBe(expected)
+    }
+    )
+    test("You should not survive if time is less than the number of lions",
+    function(){
+        //Arrange
+        const ammo = 12
+        const time = 3
+        const numberOfLions = 10
+        const expected = "Sorry!, you are dead, you've run of time "
+        //Act
+        const actual = lionAttack(ammo, time, numberOfLions);
+        //Assert
+        expect(actual).toBe(expected)
+    }
+    )
+
+
+})
+    
+    
