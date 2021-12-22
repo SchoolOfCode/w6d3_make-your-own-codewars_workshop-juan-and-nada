c3bca545ca37481b8c7101d2d572074d1bdadec5
shuamp <castel.davila@gmail.com>
2021-12-22 15:52:45 +0000

    task 1 done

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 /* 
 👉 Write your kata here!
-*/
+// 
+//scenario: 
+you find yourself alone in the jungle facing a group of lions ready to attack you, 
+but luckly you have a rifle 
+with some ammo. In order to survive you have to kill all the lions before your
+ ammo and time run out otherwise you are 
+dead. You can only shoot one lion per second.
+Create a function that  predict the result which takes in the number of lions, 
+the time you have and number of ammo.
+If you died, it will return "sorry!, you are dead".
+If not, it will return "congratulations! you survived!"
+
+you died if...
+ammo < lions 
+ time < lions
+ you live if...
+ ammo >= lions and time => lions
+
+// rifle with ammo
+// number of lions 
+// time  15 seconds
 
 //👉 Write the function your CodeWarriors will start with below here:
---
+++ plan.md
@@ -0,0 +1,6 @@
+1 scenario: 
+
+you are being attack by lions
+rifle with ammo
+number of lions 
+another paramete could be time or distance (integer)
